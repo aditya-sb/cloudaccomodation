1e6ba9ebced55a2a6a7d306e1068843a35eb9cc0
himanshubhadoria2727 <iamvikashtomar@gmail.com>
2025-01-10 17:48:43 +0530

    deploy error fixed

--- frontend/src/app/property/BookingDetails.tsx
+++ frontend/src/app/property/BookingDetails.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { FaAngleDown, FaArrowRight, FaMoneyBillWave, FaMoneyCheck, FaQuestion, FaQuestionCircle } from "react-icons/fa";
+import { FaAngleDown, FaArrowRight, FaMoneyBillWave, FaQuestionCircle } from "react-icons/fa";
 import {  FaMoneyBillTransfer } from "react-icons/fa6";
 
 const BookingForm = ({ price }: { price: string }) => {
